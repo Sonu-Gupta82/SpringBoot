0c5c143624859dc39c132b693a8d7d51e6df0bd2
Sonu-Gupta82 <sonugupta3239@gmail.com>
2023-12-13 12:44:37 +0530

    added js

--- hello3/src/main/java/com/hello/controller/MyController.java
+++ hello3/src/main/java/com/hello/controller/MyController.java
@@ -2,13 +2,15 @@ package com.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MyController {
 	
 	@GetMapping("/hello")
-	public String test() {
-		return "hello3";
+	public ModelAndView test() {
+		ModelAndView modelAndView = new ModelAndView("hello3");
+		return modelAndView;
 	}
 
 }
